b38bdffbf72a8140f4a521e9d0da150e0f848294
daniel40392 <daniel40392@gmail.com>
2019-11-14 19:40:50 +0000

    updated sites

--- src/components/site-content.js
+++ src/components/site-content.js
@@ -8,10 +8,16 @@ const SiteContent = () => (
       title="Sites"
       description="All things Website Related"
     />
-    <a href="https://dan-mcm.github.io/docs/"><Button>Docs</Button></a>
-    <br/>
-    <br/>
     <a href="https://music-centre.info/"><Button>Music Centre</Button></a>
+    <br/>
+    <br/>
+    <a href="https://dan-mcm.github.io/dnd/"><Button>DM's Toolkit</Button></a>
+    <br/>
+    <br/>
+    <a href="https://cat-ui.herokuapp.com/"><Button>Tap the Cat</Button></a>
+    <br/>
+    <br/>
+    <a href="https://dan-mcm.github.io/docs/"><Button>Docs</Button></a>
   </div>
 )
 
